873811942a89e0a428fbbd05ac67fa960fca4360
Vlad Skarzhevskyy <skarzhevskyy@gmail.com>
2025-07-08 00:29:44 -0400

    configure minimal application

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.concurrent.Callable;
 
-@SpringBootApplication
-public class DemoApplication {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.IVersionProvider;
+import picocli.CommandLine.Option;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+@Command(name = "demo", mixinStandardHelpOptions = true, versionProvider = DemoApplication.ManifestVersionProvider.class,
+        description = "A simple demo application.")
+public class DemoApplication implements Callable<Integer> {
 
+    @Option(names = {"-n", "--name"}, description = "Your name.", defaultValue = "World")
+    private String name;
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new DemoApplication()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Hello, " + name + "!");
+        return 0;
+    }
+
+    static class ManifestVersionProvider implements IVersionProvider {
+        public String[] getVersion() throws Exception {
+            Package aPackage = DemoApplication.class.getPackage();
+            String implementationVersion = aPackage != null ? aPackage.getImplementationVersion() : null;
+            if (implementationVersion == null) {
+                return new String[]{"No version information available"};
+            }
+            return new String[]{implementationVersion};
+        }
+    }
 }
